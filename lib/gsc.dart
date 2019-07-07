7c31556a3248f1fd0077863a1d27d1faa1d1da15
凌海辉 <linghaihui@licaigc.com>
2019-07-08 03:00:09 +0800

    first commit

---
+++ lib/gsc.dart
@@ -0,0 +1,59 @@
+class Gsc {
+  int id;
+  String workTitle;
+  String workAuthor;
+  String content;
+  String workDynasty;
+  String layout;
+  String translation;
+  String intro;
+  String foreword;
+  String appreciation;
+  String shortContent;
+
+  void setShortContent(){
+    // 句号
+    var periodIndex = this.content.indexOf("。");
+    if (periodIndex == -1) {
+        // 感叹号
+        var exclamatoryMarkIndex = this.content.indexOf("！");
+        if (exclamatoryMarkIndex == -1) {
+            // 问号
+            var questionMarkIndex = this.content.indexOf("？");
+            this.shortContent = this.content.substring(
+                0,
+                questionMarkIndex + 1
+            );
+        } else
+            [
+                (this.shortContent = this.content.substring(
+                    0,
+                    exclamatoryMarkIndex + 1
+                ))
+            ];
+    } else {
+        this.shortContent = this.content.substring(0, periodIndex + 1);
+    }
+  }
+
+  Gsc(Map gsc){
+    this.id = gsc["id"];
+    this.content = gsc["content"];
+    this.workAuthor = gsc["work_author"];
+    this.workTitle = gsc["work_title"];
+    this.workDynasty = gsc["work_dynasty"];
+    this.layout = gsc["layout"];
+    this.translation = gsc["translation"];
+    this.intro = gsc["intro"];
+    this.foreword = gsc["foreword"];
+    this.appreciation = gsc["appreciation"];
+    setShortContent();
+
+    if (this.layout == 'indent') {
+          this.content =  "　　" + this.content.replaceAll(new RegExp(r"\n|\t"), "\n"+ "　　");
+      }
+    if(this.translation.length > 0){
+      this.translation =  "　　" + this.translation.replaceAll(new RegExp(r"\n|\t"), "\n"+ "　　");
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,350 @@
+import 'dart:convert';
+import 'dart:io';
+import 'gsc.dart';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'i古诗词',
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primaryColor: Colors.cyan),
+      home: MyHomePage(title: 'i古诗词'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Gsc> gscList = [];
+  var loading = true;
+  HttpClient httpClient = new HttpClient();
+  final homeAip = "https://igsc.wx.haihui.site/songci/index/all/b";
+  final searchAip =
+      "https://igsc.wx.haihui.site/songci/query/{inputText}/main/b";
+
+  var editController = TextEditingController();
+
+  void getHomeGsc() async {
+    this.gscList = [];
+    this.loading = true;
+    setState(() {});
+    HttpClientRequest request =
+        await httpClient.getUrl(Uri.parse(this.homeAip));
+    HttpClientResponse response = await request.close();
+    var resp = await response.transform(utf8.decoder).join();
+    var gscs = jsonDecode(resp)["data"]["data"];
+    this.gscList = [];
+    for (var i = 0; i < gscs.length; i++) {
+      this.gscList.add(Gsc(gscs[i]));
+    }
+    this.loading = false;
+    setState(() {});
+  }
+
+  void goToDetail(Gsc gsc) {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => new GscDetailScreen(gsc)),
+    );
+  }
+
+  List<Widget> renderListView() {
+    if(this.loading){
+      return getProgressDialog();
+    }
+    List<Widget> result = [];
+    for (var i = 0; i < this.gscList.length; i++) {
+      var gsc = this.gscList[i];
+      result.add(new GestureDetector(
+          onTap: () {
+            goToDetail(gsc);
+          },
+          onDoubleTap: () {
+            goToDetail(gsc);
+          },
+          child: Column(
+            children: <Widget>[
+              Flex(direction: Axis.horizontal, children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: new EdgeInsets.all(8),
+                        child: Text(
+                          gsc.workTitle,
+                          softWrap: true,
+                          maxLines: 4,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(height: 1.5, fontFamily: "songkai"),
+                        ),
+                      ),
+                      Padding(
+                        padding: new EdgeInsets.all(8),
+                        child: Text(
+                          gsc.shortContent,
+                          softWrap: true,
+                          maxLines: 4,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(height: 1.5, fontFamily: "songkai"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Padding(
+                        padding: new EdgeInsets.all(8),
+                        child: Text(
+                          "",
+                          style: TextStyle(height: 1.5, fontFamily: "songkai"),
+                        ),
+                      ),
+                      Padding(
+                        padding: new EdgeInsets.all(8),
+                        child: Text(
+                          "【" +
+                              gsc.workDynasty +
+                              "】" +
+                              gsc.workAuthor.toString(),
+                          textAlign: TextAlign.end,
+                          style: TextStyle(height: 1.5, fontFamily: "songkai"),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ]),
+              Divider(height: 10.0, indent: 8.0, color: Colors.grey),
+            ],
+          )));
+    }
+    return result;
+  }
+
+  @override
+  void initState() {
+    // 初始化获取gsc
+    getHomeGsc();
+    super.initState();
+  }
+
+  void search() async {
+    this.gscList = [];
+    this.loading = true;
+    setState(() {});
+    var inputText = this.editController.text.trim();
+    Uri uri;
+    if (inputText.length == 0) {
+      uri = Uri.parse(homeAip);
+    } else {
+      uri = Uri.parse(searchAip.replaceAll("{inputText}", inputText));
+    }
+    HttpClientRequest request = await httpClient.getUrl(uri);
+    HttpClientResponse response = await request.close();
+    var resp = await response.transform(utf8.decoder).join();
+    var gscs = jsonDecode(resp)["data"]["data"];
+    this.gscList = [];
+    for (var i = 0; i < gscs.length; i++) {
+      this.gscList.add(Gsc(gscs[i]));
+    }
+    this.loading = false;
+    setState(() {});
+  }
+
+  getProgressDialog(){
+    List<Widget> result = [];
+    result.add(
+      Center(child:
+          new CircularProgressIndicator()
+      )
+    );
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: new Align(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          alignment: Alignment.topLeft,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            // Column is also layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: 
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10, right: 10, top: 1, bottom: 0),
+                      child: TextFormField(
+                      autofocus: false,
+                      style: TextStyle(color: Colors.blueGrey),
+                      strutStyle: StrutStyle(fontStyle: FontStyle.italic),
+                      controller: editController,
+                    ),
+                    )
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () => {
+                      search()
+                      },
+                  )
+                ],
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10, right: 10, top: 10, bottom: 0),
+                      child: Text("搜索结果({num})"
+                        .replaceAll("{num}", this.gscList.length.toString()), style: TextStyle(fontWeight: FontWeight.w600),),
+                    )
+                    
+                  ]),
+              Expanded(
+                child: ListView(
+                    shrinkWrap: true,
+                    primary: true,
+                    padding: const EdgeInsets.only(
+                        left: 0, right: 10, top: 10, bottom: 10),
+                    children: renderListView()),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class GscDetailScreen extends StatelessWidget {
+  final Gsc gsc;
+
+  GscDetailScreen(this.gsc);
+
+  final TextStyle style =
+      TextStyle(height: 1.5, fontFamily: "songkai", fontSize: 16);
+  final TextStyle styleTranslation =
+      TextStyle(height: 1.5, fontFamily: "songkai", fontSize: 14);
+
+  Widget renderTranslation(Gsc gsc) {
+    if (gsc.translation.length > 0) {
+      return Text(
+        "\n【翻译】\n" + this.gsc.translation,
+        style: styleTranslation,
+      );
+    } else {
+      return Text("");
+    }
+  }
+
+  Widget renderContent(Gsc gsc) {
+    if (gsc.layout == "center") {
+      return Text(
+        this.gsc.content,
+        style: style,
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return Text(
+        this.gsc.content,
+        style: style,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text(this.gsc.workTitle),
+        ),
+        body: Center(
+            //Text(this.gsc.content, style: TextStyle(height: 1.5, fontFamily: "songkai"))
+            child: ListView(
+          padding: EdgeInsets.all(10),
+          //shrinkWrap: true,
+          children: <Widget>[
+            Text(
+              this.gsc.workTitle,
+              style: style,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "【" + this.gsc.workDynasty + "】" + this.gsc.workAuthor,
+              style: style,
+              textAlign: TextAlign.center,
+            ),
+            renderContent(this.gsc), // 正文
+            Padding(
+                padding: EdgeInsets.all(8), child: renderTranslation(this.gsc)),
+          ],
+        )));
+  }
+}
